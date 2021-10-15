4a7a717c259004c05fd1118a7862fc218aa6ab13
Lilian SOLER <74048928+LilianSOLER@users.noreply.github.com>
2021-10-15 19:00:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+	<meta charset='utf-8'>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Site de Lilian Soler</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<?php include("php/script.php"); ?>
+</head>
+
+<body>
+	<div class='tout'>
+		<?php include("php/newHeader.php"); ?>
+		<?php include("php/section.php"); ?>
+		<?php include("php/user_nbr_live.php"); ?>
+		<p><a style='color:white;' href="https://paypal.me/LilianSoler?locale.x=fr_FR">Soutenir le créateur</a>
+		<p>
+	</div>
+	<?php include("php/newSocial.php"); ?>
+
+	<!-- 728x90_btf  Leader board-->
+	<ins data-zone="234867" class="byadthink"></ins>
+	<script type="text/javascript" async src="//ad.adxcore.com/adjs_r.php?async&what=zone:234867&inf=no"></script>
+
+</body>
+
+</html>
