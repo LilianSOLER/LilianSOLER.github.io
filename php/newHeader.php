5a632835016168dfea2855a1c1ac37cd6db57d56
Lilian SOLER <74048928+LilianSOLER@users.noreply.github.com>
2021-10-15 18:57:23 +0200

    Add files via upload

---
+++ php/newHeader.php
@@ -0,0 +1,76 @@
+<header>
+  <div class="header">
+    <a href="https://didelo.fr" class="logo">SOLER Lilian</a>
+    <div class="header-right">
+      <div class="topnav" id="myTopnav">
+        <a href="https://didelo.fr" class="active">Accueil</a>
+        <div class="dropdown">
+          <button class="dropbtn">Polytech
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a title="Arduino: Programmes Divers et Corrections des TD" href="https://didelo.fr/lilian/peip2/arduino/">PEIP2: Arduino</a>
+            <a title="Introduction au Web (HTML et CSS) : Correcttions des TD" href="https://didelo.fr/lilian/peip2/web/">PEIP2: Intro au Web</a>
+            <a title="Applications du Web (PHP et JS) : Correcttions des TD" href="https://didelo.fr/lilian/peip2/applicationWeb/">PEIP2: Applications du Web</a>
+            <a title="Introduction à la programmation en C : Correction des différents exercises" href="https://didelo.fr/lilian/info3/progC/">INFO3: Programmation en C</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="dropbtn">Teacher
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="http://users.polytech.unice.fr/~pmasson/Enseignement.htm" target="_blank">Pascal Masson(Arduino)</a>
+            <a href="http://users.polytech.unice.fr/~vg/" target="_blank">Vincent Granet(POO JAVA)</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="dropbtn">Cours
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a title="Cours particuliers pour Amandine" href="https://didelo.fr/lilian/cours/cours_amandine.php">Amandine</a>
+            <a title="Cours particuliers pour Salome" href="https://didelo.fr/lilian/cours/cours_salome.php">Salom&eacute;</a>
+            <a title="Cours particuliers pour Slimane" href="https://didelo.fr/lilian/cours/cours_slimane.php">Slimane</a>
+            <a title="Ressources pour les cours particuliers" href="https://didelo.fr/lilian/cours/ressource.php">Ressources</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="dropbtn">Entrainement WEB
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a title="Entrainement au code en HTML et CSS" href="https://didelo.fr/lilian/entrainement/_html/">HTML et CSS</a>
+            <a title="Entrainement au code CSS et ses animations" href="https://didelo.fr/lilian/entrainement/_css/">Animation CSS</a>
+            <a title="Entrainement au code en PHP et MyQSL" href="https://didelo.fr/lilian/entrainement/_php/">PHP et MyQSL</a>
+            <a title="Entrainement au code en PHP orienté objet" href="https://didelo.fr/lilian/entrainement/_php_poo">PHP orienté objet</a>
+            <a title="Entrainement au code en JavaScript" href="https://didelo.fr/lilian/entrainement/_js/">JavaScript</a>
+            <a title="Entrainement au Framework Bootstrap" href="https://didelo.fr/lilian/entrainement/_bootstrap/">Bootstrap</a>
+            <a title="Différents projets d'entraînement" href="https://didelo.fr/projet_lilian/">Diff&eacute;rents projets d'entraînement</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <button class="dropbtn">Mes infos
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a title="Curriculum Vitae  long de SOLER Lilian" href="https://didelo.fr/lilian/media/image/cv.pdf">CV long </a>
+            <a title="Curriculum Vitae court de SOLER Lilian" href="https://liliansoler.didelo.fr">CV court</a>
+          </div>
+        </div>
+        <!-- <a title="Site d'Elisa Roux'" href="https://elisaroux.didelo.fr">Elisa ROUX</a> -->
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+      </div>
+      <script>
+        function myFunction() {
+          var x = document.getElementById("myTopnav");
+          if (x.className === "topnav") {
+            x.className += " responsive";
+          } else {
+            x.className = "topnav";
+          }
+        }
+      </script>
+    </div>
+  </div>
+</header>
---
+++ php/script.php
@@ -0,0 +1,40 @@
+<meta name="author" content="SOLER Lilian" />
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+  (function() {
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/5f9acad97f0a8e57c2d8348d/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+  })();
+</script>
+<!--End of Tawk.to Script-->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5714Y07X47"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-5714Y07X47');
+</script>
+
+<!-- Logo -->
+<link rel="apple-touch-icon" sizes="180x180" href="https://didelo.fr/lilian/media/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://didelo.fr/lilian/media/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://didelo.fr/lilian/media/icons/favicon-16x16.png">
+<link rel="manifest" href="https://didelo.fr/lilian/site.webmanifest">
+
+<!-- Font awesome logo -->
+<script src="https://use.fontawesome.com/cebfcad694.js"></script>
+<script src="https://kit.fontawesome.com/8e3c581117.js" crossorigin="anonymous"></script>
